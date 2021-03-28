07e315e361ca0432a412559437bb3e38feacc9ca
UJWAL PANKAJ JAIN <57255611+veeruj2000@users.noreply.github.com>
2021-03-28 10:24:05 +0530

    Add files via upload

---
+++ File username display.py
@@ -0,0 +1,8 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> filename = input("Input the Filename: ")
+Input the Filename: java.py
+>>> f_extns = filename.split(".")
+>>> print ("The extension of the file is : " + repr(f_extns[-1]))
+The extension of the file is : 'py'
+>>> 
---
+++ Radius of Circle.py
@@ -0,0 +1,8 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import pi
+>>> r = float(input ("Input the radius of the circle : "))
+Input the radius of the circle : 1.1
+>>> print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+The area of the circle with radius 1.1 is: 3.8013271108436504
+>>> 
